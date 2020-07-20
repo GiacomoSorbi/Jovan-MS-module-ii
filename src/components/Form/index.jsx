b0262e2ba6330be9ec6d9043568742e2779db8f5
Jovan <jms.uk.2001@gmail.com>
2020-07-20 03:52:28 +0100

    bug fixes

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,35 +1,124 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { useForm } from "react-hook-form";
-
 import "./Form.css";
 
-function Form() {
-	const { register, handleSubmit, errors } = useForm();
-	const onSubmit = (data) => {
-		console.log(data);
-	};
+class Form extends React.Component {
+	constructor(props) {
+		super(props);
 
-	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<label>Your Name</label>
-			<input name='name' ref={register} />
-			<label>Your Email Address</label>
-			<input
-				name='exampleRequired'
-				ref={register({ required: true, maxLength: 10 })}
-			/>
-			{errors.exampleRequired && (
-				<p className='errortext'>This field is required</p>
-			)}
-			<label>Your Message</label>
-			<input name='message' ref={register} />
-			<input type='submit' />
-		</form>
-	);
+		this.state = {
+			fields: {},
+			errors: {},
+		};
+	}
+
+	handleValidation() {
+		let fields = this.state.fields;
+		let errors = {};
+		let formIsValid = true;
+
+		if (!fields["name"]) {
+			formIsValid = false;
+			errors["name"] = "Cannot be empty";
+		}
+
+		if (typeof fields["name"] !== "undefined") {
+			if (!fields["name"].match(/^[a-zA-Z]+$/)) {
+				formIsValid = false;
+				errors["name"] = "Only letters";
+			}
+		}
+
+		if (!fields["email"]) {
+			formIsValid = false;
+			errors["email"] = "Cannot be empty";
+		}
+
+		if (typeof fields["email"] !== "undefined") {
+			let lastAtPos = fields["email"].lastIndexOf("@");
+			let lastDotPos = fields["email"].lastIndexOf(".");
+
+			if (
+				!(
+					lastAtPos < lastDotPos &&
+					lastAtPos > 0 &&
+					fields["email"].indexOf("@@") == -1 &&
+					lastDotPos > 2 &&
+					fields["email"].length - lastDotPos > 2
+				)
+			) {
+				formIsValid = false;
+				errors["email"] = "Email is not valid";
+			}
+		}
+
+		this.setState({ errors: errors });
+		return formIsValid;
+	}
+
+	contactSubmit(e) {
+		e.preventDefault();
+		if (this.handleValidation()) {
+			alert("Form submitted");
+		} else {
+			alert("Form has errors.");
+		}
+	}
+
+	handleChange(field, e) {
+		let fields = this.state.fields;
+		fields[field] = e.target.value;
+		this.setState({ fields });
+	}
+
+	render() {
+		return (
+			<div>
+				<form name='contactform' onSubmit={this.contactSubmit.bind(this)}>
+					<div>
+						<input
+							ref='name'
+							type='text'
+							size='30'
+							placeholder='Name'
+							onChange={this.handleChange.bind(this, "name")}
+							value={this.state.fields["name"]}
+						/>
+						<span className='errortext'>{this.state.errors["name"]}</span>
+						<br />
+						<input
+							refs='email'
+							type='text'
+							size='30'
+							placeholder='Email'
+							onChange={this.handleChange.bind(this, "email")}
+							value={this.state.fields["email"]}
+						/>
+						<span className='errortext'>{this.state.errors["email"]}</span>
+						<br />
+					</div>
+					<div>
+						<textarea
+							refs='message'
+							cols='28'
+							rows='5'
+							placeholder='Message'
+							onChange={this.handleChange.bind(this, "message")}
+						>
+							{this.state.fields["message"]}
+						</textarea>
+					</div>
+					<div>
+						<button id='submit' value='Submit'>
+							Send Message
+						</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Form />, rootElement);
+ReactDOM.render(<Form />, document.querySelector("#root"));
 
 export default <Form></Form>;
